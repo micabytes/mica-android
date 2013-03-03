4f752778c492c73bc2ca3c5a019473a3a05c905d
Michael Akinde <michael.akinde@micabyte.com>
2013-03-03 15:31:01 +0100

    Xml parsing utility class.

---
+++ src/com/micabyte/android/util/XmlParser.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2013 MicaByte Systems
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.micabyte.android.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * A static function that does all the repetitive lifting for getting an XML reader from the
+ * SAXParser and running the handler. Extend DefaultHandler to do the actual decoding work. 
+ * 
+ * @author micabyte
+ */
+public class XmlParser {
+
+	public static void parseXml(InputStream istream, DefaultHandler handler) {
+		try {
+			// Get a SAXParser from the SAXPArserFactory.
+			SAXParserFactory spf = SAXParserFactory.newInstance();
+			SAXParser sp;
+			sp = spf.newSAXParser();
+			// Get the XMLReader of the SAXParser we created.
+			XMLReader xr;
+			xr = sp.getXMLReader();
+			// Create a new ContentHandler and apply it to the XML-Reader
+			xr.setContentHandler(handler);
+			xr.parse(new InputSource(istream));
+		}
+		catch (SAXException e) {
+			e.printStackTrace();
+		}
+		catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
