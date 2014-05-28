6a52ff6bd874150af46726bcbbad47f3b44dc697
Michael Akinde <michael.akinde@micabyte.com>
2014-05-28 10:58:24 +0200

    Removed getRoundedCornerBitmap. Too memory-consuming.

--- src/com/micabyte/android/graphics/ImageHandler.java
+++ src/com/micabyte/android/graphics/ImageHandler.java
@@ -90,25 +90,6 @@ public class ImageHandler {
         return ret;
     }
 
-    // TODO: These two functions still need to be generalized
-    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
-        Bitmap output =
-                Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-        final int color = 0xff424242;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-        final RectF rectF = new RectF(rect);
-        final float roundPx = 12;
-        paint.setAntiAlias(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(color);
-        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
-        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, rect, rect, paint);
-        return output;
-    }
-
     public Bitmap getSceneBitmap(int bkg, int left, int right) {
         Bitmap bitmap = get(bkg);
         Bitmap output =
