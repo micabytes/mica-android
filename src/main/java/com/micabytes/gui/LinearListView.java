d0d6557bef83c518421f59059d67ad6bc0b751f9
micabytes <michael.akinde@micabytes.com>
2017-05-28 02:41:38 +0200

    Adding Handlers to GameView and ShipStatus.

--- src/main/java/com/micabytes/gui/LinearListView.java
+++ src/main/java/com/micabytes/gui/LinearListView.java
@@ -3,11 +3,14 @@ package com.micabytes.gui;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.database.DataSetObserver;
+import android.databinding.BindingAdapter;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -15,77 +18,77 @@ import java.util.List;
 
 // android:orientation
 @SuppressWarnings("ClassUnconnectedToPackage")
-public class LinearListView extends LinearLayout
-{
-    private Adapter adapter;
-    @SuppressWarnings("ThisEscapedInObjectConstruction") private final Observer observer = new Observer(this);
+public class LinearListView extends LinearLayout {
+  private Adapter adapter;
+  @SuppressWarnings("ThisEscapedInObjectConstruction") private final Observer observer = new Observer(this);
 
-    public LinearListView(Context context)
-    {
-        super(context);
+  public LinearListView(Context context) {
+    super(context);
+  }
+
+  public LinearListView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  public LinearListView(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+  }
+
+  public void setAdapter(Adapter adp) {
+    if (adapter != null) {
+      adapter.unregisterDataSetObserver(observer);
     }
-
-    public LinearListView(Context context, AttributeSet attrs)
-    {
-        super(context, attrs);
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public LinearListView(Context context, AttributeSet attrs, int defStyle)
-    {
-        super(context, attrs, defStyle);
-    }
-
-    public void setAdapter(Adapter adp)
-    {
-        if (adapter != null) {
-          adapter.unregisterDataSetObserver(observer);
-        }
-
-        adapter = adp;
-      adp.registerDataSetObserver(observer);
-      observer.onChanged();
-    }
+    adapter = adp;
+    adp.registerDataSetObserver(observer);
+    observer.onChanged();
+  }
 
   Adapter getAdapter() {
     return adapter;
   }
 
-  private static final class Observer extends DataSetObserver
-    {
-        final LinearListView listView;
+  @BindingAdapter("bind:adapter")
+  public  static void bindList(LinearListView view, BaseAdapter adapter) {
+    view.setAdapter(adapter);
+  }
 
-        private Observer(LinearListView lw)
-        {
-            listView = lw;
-        }
+  private static final class Observer extends DataSetObserver {
+    final LinearListView listView;
 
-        @SuppressWarnings("MethodWithMultipleLoops")
-        @Override
-        public void onChanged()
-        {
-            List<View> oldViews = new ArrayList<>(listView.getChildCount());
-
-            for (int i = 0; i < listView.getChildCount(); i++)
-                oldViews.add(listView.getChildAt(i));
-
-            Iterator<View> itr = oldViews.iterator();
-
-            listView.removeAllViews();
-
-            for (int i = 0; i < listView.getAdapter().getCount(); i++)
-            {
-                View convertView = itr.hasNext() ? itr.next() : null;
-                listView.addView(listView.getAdapter().getView(i, convertView, listView));
-            }
-            super.onChanged();
-        }
-
-        @Override
-        public void onInvalidated()
-        {
-            listView.removeAllViews();
-            super.onInvalidated();
-        }
+    private Observer(LinearListView lw) {
+      listView = lw;
     }
+
+    @SuppressWarnings("MethodWithMultipleLoops")
+    @Override
+    public void onChanged() {
+      List<View> oldViews = new ArrayList<>(listView.getChildCount());
+
+      for (int i = 0; i < listView.getChildCount(); i++)
+        oldViews.add(listView.getChildAt(i));
+
+      Iterator<View> itr = oldViews.iterator();
+
+      listView.removeAllViews();
+
+      for (int i = 0; i < listView.getAdapter().getCount(); i++) {
+        View convertView = itr.hasNext() ? itr.next() : null;
+        listView.addView(listView.getAdapter().getView(i, convertView, listView));
+      }
+      super.onChanged();
+    }
+
+    @Override
+    public void onInvalidated() {
+      listView.removeAllViews();
+      super.onInvalidated();
+    }
+  }
+
+  @BindingAdapter("bind:adapter")
+  public  static void bindList(ListView view, BaseAdapter adapter) {
+    view.setAdapter(adapter);
+  }
+
 }
