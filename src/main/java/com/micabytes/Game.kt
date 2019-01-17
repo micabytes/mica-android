1d69c9ef3476068e645959b928dfd03660b9544a
Michael Akinde <michael.akinde@micabytes.com>
2019-01-17 11:58:49 +0100

    P&T2 v0.317

--- src/main/java/com/micabytes/Game.kt
+++ src/main/java/com/micabytes/Game.kt
@@ -1,19 +1,29 @@
 package com.micabytes
 
+import android.app.Activity
 import android.app.Application
+import com.micabytes.app.BaseActivityCallback
 
 /* The Game Application */
 class Game : Application() {
 
+  init {
+    instance = this
+  }
+
+  val mFTActivityLifecycleCallbacks = BaseActivityCallback()
+
   override fun onCreate() {
     super.onCreate()
-    instance = this
+    registerActivityLifecycleCallbacks(mFTActivityLifecycleCallbacks)
   }
 
   companion object {
     @JvmStatic
     lateinit var instance: Game
       private set
+    val currentActivity: Activity
+      get() = instance.mFTActivityLifecycleCallbacks.currentActivity!!
     var world: WorldInterface? = null
     var data: DataInterface? = null
   }
